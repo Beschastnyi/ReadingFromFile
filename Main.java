cfc4f7ba77234bee1ce7554af32f330b1608661a
Beschastnyi <vlad_beschastnyi@icloud.com>
2022-04-16 20:04:52 +0300

    Create a skeleton. Now the program read file and output it in console.

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Vlad creates project");
+    public static void main(String[] args) throws Exception{
+
+        //Create object of class File to add document in type txt
+        File file = new File("Frankenstein__Or__The_Modern_Prometheus_by_Mary_Wollstonecraft_Shelley.txt");
+
+        //Create object of class BufferedReader to be able to read file
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        //Create object of the class String to output data from txt document
+        String text;
+
+        //cycle to output data from txt document
+        while ((text = bufferedReader.readLine()) != null){
+            System.out.println(text);
+        }
     }
 }
